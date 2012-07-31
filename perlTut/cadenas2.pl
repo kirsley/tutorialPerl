386abc9317599e905da75cd3410f5f4a3211ef6e
kirsley <kirsley@gmail.com>
2012-07-31 11:16:03 +0200

    Afegir info 9

---
+++ perlTut/cadenas2.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+
+#Treball amb cadenes
+
+$cadena = "Aixo es una cadena";
+print "$cadena ";
+print substr($cadena, 8, 0, "not a ")."\n";
+print $cadena."\n";
+print substr($cadena,4,3)."\n";
+print $cadena."\n";  
