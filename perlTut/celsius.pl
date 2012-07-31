0ff377706a784b168e00142dff7823f2cdd92a8c
kirsley <kirsley@gmail.com>
2012-07-31 10:45:26 +0200

    Afegir info 2

---
+++ perlTut/celsius.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+# c2f.pl
+# A Celsius->Fahrenheit Converter
+# Print the prompt
+print "Please enter a Celsius degree > ";
+# Chop off the trailing newline character
+chomp($cel =<STDIN>);
+
+$fah = ($cel * 1.8) + 32;
+
+#resultat:
+print "El equivalent Fahrenheit de $cel graus Celsius es $fah\n";
+
