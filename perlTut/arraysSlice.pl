b0e017fa1083ad417d7e12fee532ce91b8dab188
kirsley <kirsley@gmail.com>
2012-07-31 13:17:57 +0200

    Add info 24

--- perlTut/arraysSlice.pl
+++ perlTut/arraysSlice.pl
@@ -7,3 +7,14 @@ $, = " ";
 print @{slice};
 print "\n";
 
+%Age = (
+Tom => 26,
+Peter => 51,
+Jones => 23,
+);
+@temp = delete @Age{'Tom', 'Peter'};
+$, = " ";
+print "Deleted values:", @temp, "\n";
+print "Remaining keys:", keys %Age, "\n";
+
+print "ha" x 5
